3ba1fbc603016fa73de27fbbd7113632c8b829ff
Rekha-Ahir <108225073+Rekha-Ahir@users.noreply.github.com>
2022-07-23 16:36:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express= require('express');
+const app= express();
+const bodyparser=require('body-parser')
+const path=require('path');
+
+const port =process.env.PORT||8000
+const static_path = path.join(__dirname,"../public/js");//we add path yrr
+app.use(express.static(static_path));
+app.get('/', (req, res)=>{
+   
+    const params = {}
+    res.status(200).render('index.html', params);
+});
+app.get("*",(req,res)=>{
+    
+    res.send("404 error page")
+})
+
+app.listen(port,()=>{
+    console.log(`succesfully run at port${port}`)
+})
